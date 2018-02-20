40653fe2e4e6cb90e63106a2e6a710346e5cd1a7
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-02-21 00:20:43 +0700

    Update speed

--- js/app.js
+++ js/app.js
@@ -1,77 +1,81 @@
 let client = new WebTorrent();
 
 function visAlert(vm,stat,col) {
-  vm.status = stat;
-  if(col == "war"){
-    vm.statusVisWar = true;
-    function visF(){
-      vm.statusVisWar = false;
+    vm.status = stat;
+    if(col == "war"){
+	vm.statusVisWar = true;
+	function visF(){
+	    vm.statusVisWar = false;
+	}
+	setTimeout(visF, 2000);
+    }else if(col == "suc"){
+	vm.statusVisSuc = true;
+	function visF(){
+	    vm.statusVisSuc = false;
+	}
+	setTimeout(visF, 2000);
     }
-    setTimeout(visF, 2000);
-  }else if(col == "suc"){
-    vm.statusVisSuc = true;
-    function visF(){
-      vm.statusVisSuc = false;
-    }
-    setTimeout(visF, 2000);
-  }
 }
 
 let app = new Vue({
-  el: '#main',
-  data:{
-    main: true,
-    loader: false,
-    status: '',
-    magnetUrlDown: '',
-    downSpeed: '',
-    upSpeed: '',
-    statusVisSuc: false,
-    statusVisWar: false,
-    UpVis: false,
-    DownVis: false,
-  },
-  methods:{
-    upload: function(event) {
-      let vm = this;
-      let file = event.target.files[0];
-      if(file != null){
-        visAlert(vm,'The distribution began!',"suc");
-        client.seed(file, function (torrent) {
-          vm.fileNameUp = file.name;
-          vm.fileNameTDown = `${file.name}.torrent`;
-          vm.magnetUp = torrent.magnetURI;
-          vm.hashUp = `Hash:${torrent.infoHash}`;
-          vm.torrentFileUp = torrent.torrentFileBlobURL;
-          vm.UpVis = true;
-        })
-      }else{
-        visAlert(vm,'File not found',"war");
-      }
-    },
-    download:function() {
-      let vm = this;
-      if((this.magnetUrlDown != null) && this.magnetUrlDown.length > 0){
-        visAlert(vm,'Download has started!',"suc");
-        client.add(this.magnetUrlDown, function(torrent) {
-          vm.hashDown = `Hash:${torrent.infoHash}`;
-          var file = torrent.files[0];
-          vm.fileNameDown = file.name;
-          file.getBlobURL(function(err, url) {
-            if (err) throw err
-            vm.fileUrlDown = url;
-            vm.DownVis = true;
-            visAlert(vm,'Loading is complete!',"suc");
-          })
-        })
-      }else{
-        visAlert(vm,'Add a magnet link',"war");
-      }
-    },
-  },
-})
-
-setInterval(function() {
-  app.downSpeed = client.downloadSpeed;
-  app.upSpeed = client.uploadSpeed;
-},500);
+		      el: '#main',
+		      data:{
+			  main: true,
+			  loader: false,
+			  status: '',
+			  magnetUrlDown: '',
+			  speed: {
+			      up: 0,
+			      down: 0,
+			  },
+			  statusVisSuc: false,
+			  statusVisWar: false,
+			  UpVis: false,
+			  DownVis: false,
+		      },
+		      mounted: function(){
+			  let vm = this
+			  setInterval(function() {
+					  vm.speed.down = client.downloadSpeed
+					  vm.speed.up = client.uploadSpeed
+				      },500)
+		      },
+		      methods:{
+			  upload: function(event) {
+			      let vm = this;
+			      let file = event.target.files[0];
+			      if(file != null){
+				  visAlert(vm,'The distribution began!',"suc");
+				  client.seed(file, function (torrent) {
+						  vm.fileNameUp = file.name;
+						  vm.fileNameTDown = `${file.name}.torrent`;
+						  vm.magnetUp = torrent.magnetURI;
+						  vm.hashUp = `Hash:${torrent.infoHash}`;
+						  vm.torrentFileUp = torrent.torrentFileBlobURL;
+						  vm.UpVis = true;
+					      })
+			      }else{
+				  visAlert(vm,'File not found',"war");
+			      }
+			  },
+			  download:function() {
+			      let vm = this;
+			      if((this.magnetUrlDown != null) && this.magnetUrlDown.length > 0){
+				  visAlert(vm,'Download has started!',"suc");
+				  client.add(this.magnetUrlDown, function(torrent) {
+						 vm.hashDown = `Hash:${torrent.infoHash}`;
+						 var file = torrent.files[0];
+						 vm.fileNameDown = file.name;
+						 file.getBlobURL(function(err, url) {
+								     if (err) throw err
+								     vm.fileUrlDown = url;
+								     vm.DownVis = true;
+								     visAlert(vm,'Loading is complete!',"suc");
+								 })
+					     })
+			      }else{
+				  visAlert(vm,'Add a magnet link',"war");
+			      }
+			  },
+		      },
+		  })
--- js/speed.js
+++ js/speed.js
@@ -1,8 +1,8 @@
 Vue.component('speed', {
 		  template: '<div class="speed" v-show="main" style="display: none;">' +
 		      'Statistics:' +
-		      '[Up&nbsp;<i class="ion-ios-arrow-thin-up"></i>{{up/1000}}&nbsp;kB/sec]&nbsp;' + 
-		      '[Down&nbsp;<i class="ion-ios-arrow-thin-down"></i>{{down/1000}}&nbsp;kB/sec]' +
+		      '[Up&nbsp;<i class="ion-ios-arrow-thin-up"></i>{{speed.up/1000}}&nbsp;kB/sec]&nbsp;' + 
+		      '[Down&nbsp;<i class="ion-ios-arrow-thin-down"></i>{{speed.down/1000}}&nbsp;kB/sec]' +
 		      '</div>',
-		  props: ['up', 'down'],
+		  props: ['main', 'speed'],
 	      })
