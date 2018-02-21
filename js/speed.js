e7bac70d74a4d05bd174c9b3be8d382837c6f9e5
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-02-21 21:20:58 +0700

    Fix style page

--- js/speed.js
+++ js/speed.js
@@ -1,6 +1,6 @@
 Vue.component('speed', {
 		  template: '<div class="speed" v-show="main" style="display: none;">' +
-		      'Statistics:' +
+		      'Speed:' +
 		      '[Up&nbsp;<i class="ion-ios-arrow-thin-up"></i>{{speed.up/1000}}&nbsp;kB/sec]&nbsp;' + 
 		      '[Down&nbsp;<i class="ion-ios-arrow-thin-down"></i>{{speed.down/1000}}&nbsp;kB/sec]' +
 		      '</div>',
