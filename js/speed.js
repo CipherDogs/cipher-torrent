19572e340d61b201cb9dd78f3f28dbd53cd2018c
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-02-23 02:09:16 +0700

    New style speed component

--- js/speed.js
+++ js/speed.js
@@ -1,8 +1,7 @@
 Vue.component('speed', {
 		  template: '<div class="speed" v-show="main" style="display: none;">' +
-		      'Speed:' +
-		      '[Up&nbsp;<i class="ion-ios-arrow-thin-up"></i>{{speed.up/1000}}&nbsp;kB/sec]&nbsp;' + 
-		      '[Down&nbsp;<i class="ion-ios-arrow-thin-down"></i>{{speed.down/1000}}&nbsp;kB/sec]' +
+		      '<i class="ion-ios-arrow-thin-up"></i>&nbsp;<b>{{speed.up/1000}}</b>&nbsp;kB/sec&nbsp;' + 
+		      '<i class="ion-ios-arrow-thin-down"></i>&nbsp;<b>{{speed.down/1000}}</b>&nbsp;kB/sec&nbsp;' +
 		      '</div>',
 		  props: ['main', 'speed'],
 	      })
