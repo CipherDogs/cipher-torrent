467c41beb5ed5a190a24aae203c2a6b05afc659e
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-02-26 11:13:38 +0700

    Add footer template component

---
+++ template/footer.js
@@ -0,0 +1,20 @@
+Vue.component('footline', {
+		  template: '<div class="footer">' +
+		      '<div class="left">' +
+		      '<a href="https://github.com/deadblackclover" target="_blank">' +
+		      '<i class="ion-social-github"></i>&nbsp;DEADBLACKCLOVER' +
+		      '</a>' +
+		      '</div>' +
+		      '<slot></slot>' +
+		      '<div class="right">' +
+		      '<a href="http://js.org" target="_blank" title="JS.ORG | JavaScript Community">' +
+		      '<img src="http://logo.js.org/dark_horz.png" width="102" alt="JS.ORG Logo" style="width:40px;"/>' +
+		      '</a>' +
+		      '<!-- alternatives [bright|dark]_[horz|vert|tiny].png (width[horz:102,vert:50,tiny:77]) -->' +
+		      '<a href="https://webtorrent.io/" target="_blank">' + 
+		      '<img src="https://webtorrent.io/img/webtorrent-small.png">' +
+		      '</a>' + 
+		      '<a href="https://vuejs.org/" target="_blank"><img src="https://vuejs.org/images/logo.png"></a>' +
+		      '</div>' +
+		      '</div>'
+	      })
