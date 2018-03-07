aef51715a4c447de454b6f2362bcd5fc412a38fe
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-03-07 21:21:51 +0700

    Create file lang

---
+++ lang/lang.js
@@ -0,0 +1,44 @@
+const local = {
+    en: {
+	btn: {
+	    seed: 'Seed File',
+	    down: 'Download',
+	    magnet: 'Magnet',
+	    torrent: 'Torrent file',
+	    file: 'Get file'
+	},
+	title: {
+	    head: 'Online and offline browser torrent client',
+	    seed: 'Seeding',
+	    down: 'Downloading'
+	},
+	text: {
+	    file: 'File name',
+	    hash: 'Hash',
+	    size: 'Size',
+	    magnet: 'Magnet Link, info hash or http/https url .torrent'
+	},
+	speed: 'kB/sec'
+    },
+    ru: {
+	btn: {
+	    seed: 'Раздать файл',
+	    down: 'Скачать',
+	    magnet: 'Magnet',
+	    torrent: 'Торрент файл',
+	    file: 'Получить файл'
+	},
+	title: {
+	    head: 'Онлайн и оффлайн браузерный торрент клиент',
+	    seed: 'Раздача',
+	    down: 'Скачивание'
+	},
+	text: {
+	    file: 'Имя файла',
+	    hash: 'Хэш',
+	    size: 'Размер',
+	    magnet: 'Magnet ссылка, хэш или ссылка на торрент файл'
+	},
+	speed: 'кБ/сек'
+    }
+}
