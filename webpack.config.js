425811f1506e5163fe0b52e3a4f33a0e1dd78b25
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-04-04 00:13:15 +0700

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: './js/app.js'
+  },
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: ['babel-loader']
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      }
+    ]
+  },
+  resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    }
+}
