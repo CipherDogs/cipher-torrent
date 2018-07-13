ec7cd7265dbe7663944f3dbc53b410e58f7a0674
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-07-13 20:20:48 +0700

    Add components

---
+++ components/App/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="preloaders" v-if="loader">
+        <img src="img/preloader.gif">
+    </div>
+    <div class="main" v-else>
+        <headline></headline>
+        <panel></panel>
+        <speed></speed>
+        <div class="blocks">
+            <upload></upload>
+            <download></download>
+        </div>
+        <notice></notice>
+        <footline></footline>
+    </div>
+</template>
+
+<script>
+import headline from '../Header/Header.vue'
+import panel from '../Panel/Panel.vue'
+import speed from '../Speed/Speed.vue'
+import upload from '../Upload/Upload.vue'
+import download from '../Download/Download.vue'
+import notice from '../Notice/Notice.vue'
+import footline from '../Footer/Footer.vue'
+
+export default {
+    data: {
+        loader: false
+    },
+    components: {
+        headline,
+        panel,
+        speed,
+        upload,
+        download,
+        notice,
+        footline
+    }
+}
+</script>
---
+++ components/Header/Header.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+    <div class="header">
+        <div class="left">
+            <a href="index.html">
+                <h1 class="title">lofiTorrent&nbsp;<i class="ion-ios-infinite-outline"></i></h1>
+            </a>
+        </div>
+        <div class="right">{{ $t("title.head") }}</div>
+    </div>
+</template>
+
+<script>
+    import { bus, client } from '../../js/bus.js'
+    import myLocal from '../../js/translation.js'
+    
+    export default{
+        i18n: myLocal
+    }
+</script>
