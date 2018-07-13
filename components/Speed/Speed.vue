e927dfaa20c3176b647441fd4c649513ff8eac51
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-07-13 22:43:56 +0700

    Add Speed component

---
+++ components/Speed/Speed.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+    <div class="speed">
+        <i class="ion-ios-arrow-thin-up"></i>&nbsp;
+        <b>{{speed.up}}</b>&nbsp;{{ $t("speed") }}&nbsp;
+        <i class="ion-ios-arrow-thin-down"></i>&nbsp;
+        <b>{{speed.down}}</b>&nbsp;{{ $t("speed") }}&nbsp;
+    </div>
+</template>
+
+<script>
+    import { bus, client } from '../../js/bus.js'
+    import myLocal from '../../js/translation.js'
+    
+    export default {
+        data() {
+            return {
+                speed: {
+                    up: 0,
+                    down: 0
+                }
+            }
+        },
+        i18n: myLocal,
+        mounted: function() {
+            let vm = this
+            setInterval(function() {
+                vm.speed.down = Number(client.downloadSpeed / 1000).toFixed(2)
+                vm.speed.up = Number(client.uploadSpeed / 1000).toFixed(2)
+            }, 500)
+        }
+    }
+</script>
