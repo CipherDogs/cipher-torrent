032f003cba002c63fb973d512cd4fbcf776a2d74
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-07-14 18:00:26 +0700

    Add Download and Upload components

---
+++ components/Download/Download.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+    <div class="block" style="right:0;">
+        <div class="block-head">{{ $t("title.down") }}</div>
+        <div class="block-body" >
+            <div v-if="down.visible">
+                <table>
+                    <tr><td class="td-name">{{ $t("text.file") }}</td><td>{{down.name}}</td></tr>
+                    <tr><td class="td-name">{{ $t("text.hash") }}</td><td>{{down.hash}}</td></tr>
+                    <tr><td class="td-name">{{ $t("text.size") }}</td><td>{{down.length}}MB</td></tr>
+                </table>
+                <p v-if="down.process.visible" class="process">In the process<br>{{ down.progress }}%</p>
+                <p v-if="down.btns.visible">
+                    <a :href="down.url" :download="down.name">
+                        <label class="btn"><i class="ion-ios-copy-outline"></i>{{ $t("btn.file") }}</label>
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { bus, client } from '../../js/bus.js'
+    import myLocal from '../../js/translation.js'
+    
+    export default{
+        i18n: myLocal,
+        props: ['down']
+    }
+</script>
